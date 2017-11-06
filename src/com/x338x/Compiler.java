f83f5bb7bfb0c44e60b87040bf8af506efdd10ee
jeff murphy <jcmurphy@jeffmurphy.org>
2017-11-06 07:14:18 -0500

    no antlr

--- src/com/x338x/Compiler.java
+++ src/com/x338x/Compiler.java
@@ -1,14 +1,4 @@
 package com.x338x;
-import org.antlr.v4.runtime.ANTLRInputStream;
-
-import org.antlr.v4.runtime.ANTLRFileStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -27,7 +17,7 @@ public class Compiler {
     private class LabelNode {
         int iNum;
         String instruction;
-        labelNode(int n, String i) {
+        LabelNode(int n, String i) {
             iNum = n;
             instruction = i;
         }
@@ -42,17 +32,6 @@ public class Compiler {
                     "    BNZ A, :l1 \n" +
                     "\n" +
                     "\n");
-
-            /*
-            InputStream stream = new ByteArrayInputStream(tp.getBytes(StandardCharsets.UTF_8));
-
-            ExprLexer lexer = new ExprLexer((org.antlr.v4.runtime.CharStream) new ANTLRInputStream(tp.toCharArray(), tp.length()));
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            ExprParser parser = new ExprParser(tokens);
-            ParseTree tree = parser.prog();
-            ParseTreeWalker walker = new ParseTreeWalker();
-            walker.walk( new ExprWalker(), tree );
-            */
 
             for (String line : tp.split("\n")) {
                 System.out.println(line);
@@ -100,43 +79,6 @@ public class Compiler {
         System.out.println(iNum + ": ADD " + dst + " " + src);
     }
 
-
-    /*
-    public class ExprWalker extends ExprBaseListener {
-        @Override
-        public void enterExpr(ExprParser.ExprContext ctx ) {
-            System.out.println("CC: " + ctx.getChildCount());
-            switch(ctx.getChildCount()) {
-                case 4:
-Object o = ctx.getChild(0);
-
-for(ParseTree p : ctx.children) {
-    System.out.println(p);
-}
-                    if(ctx.getChild(0).getText().equals("LD")) {
-                        LD(ctx.REG().toString(), ctx.ADDR().toString());
-                    }
-                    else if (ctx.getChild(0).getText().equals("ST")) {
-                        ST(ctx.REG().toString(), ctx.ADDR().toString());
-                    }
-                    else if (ctx.getChild(0).getText().equals("ADD")) {
-                        ADD(ctx.DST().toString(), ctx.SRC().toString());
-                    }
-                    line += 1;
-                    break;
-                case 2: // label: expr
-                    break;
-                case 6: // BEG BGT BLT
-                    break;
-            }
-
-        }
-
-        @Override
-        public void exitExpr(ExprParser.ExprContext ctx ) {
-        }
-    }
-*/
 
 
 }
